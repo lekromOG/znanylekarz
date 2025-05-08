8e66b839b0aaa612ad2f58febe9c62a50a8b6d95
KacperKlimas10 <10kacper.klimas.lo1@gmail.com>
2025-05-08 22:46:45 +0200

    Repaired JWT middleware

--- backend/middlewares/jwt.js
+++ backend/middlewares/jwt.js
@@ -1,17 +1,20 @@
 import jwt from 'jsonwebtoken';
 
 const authenticateToken = async (req, res, next) => {
+    try {
+        const authorization = req.get('Authorization');
+        const token = authorization.split(' ')[1]; 
 
-    const authorization = req.get('Authorization');
-    const token = authorization.split(' ')[1]; 
-
-    if (token != null) {
-        jwt.verify(token, 'privateKey', (err, user) => {
-            if (err) { return res.status(500).json({ error: `${err}` }); }
-            req.user_uuid = user.sub;
-            next();
-        });
-    } else return res.status(200).json({ role: "guest" });
+        if (token != null) {
+            jwt.verify(token, 'privateKey', (err, user) => {
+                if (err) { return res.status(500).json({ error: `${err}` }); }
+                req.user_uuid = user.sub;
+                next();
+            });
+        } else return res.status(200).json({ role: "guest" });
+    } catch (err) { 
+        return res.status(200).json({ role: "guest" }) 
+    };
 };
 
 export {
