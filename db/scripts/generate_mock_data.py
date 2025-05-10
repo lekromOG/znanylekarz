ea40d74ae53f01811f47e6c8f5c21005183f87e8
Adam Trzpis <worms3d4@tlen.pl>
2025-05-10 23:28:32 +0200

    V0.1 Done

--- db/scripts/generate_mock_data.py
+++ db/scripts/generate_mock_data.py
@@ -6,7 +6,14 @@ import argparse
 import random
 from pprint import pprint
 
+"""
+To install:
+pip3 install -r requirements.txt
+"""
+
 DEBUG_URI = "mongodb+srv://krzysio:krzysio1@cluster0.itstbt5.mongodb.net/"
+
+USAGE_STR = 'python3 generate_mock_data.py --count 1 "mongodb+srv://krzysio:krzysio1@cluster0.itstbt5.mongodb.net/" test users'
 
 def gen_user():
     """
@@ -32,21 +39,23 @@ def main():
     collection = None
 
     parser = argparse.ArgumentParser(prog="gen_mock_data", 
-                                     description="Generate random data for GoodDoctor db.",
+                                     description="Generate random data for GoodDoctor db. Example:\n" + USAGE_STR,
                                      epilog="Priopriatery software for GoodDoctor corp.")
     
-    parser.add_argument('dburi')
+    parser.add_argument('dburi', help="URI of the database")
+    parser.add_argument('dbname', help="Database name to query.")
+    parser.add_argument('collectionname', help="Name of the collection to modify.")
     # parser.add_argument('usertype') # for now ignored
-    parser.add_argument('-c', '--count')
-    parser.add_argument('-x', '--clear', action="store_true")
-    parser.add_argument('-p', '--print', action="store_true")
+    parser.add_argument('-c', '--count', help="How many entries to generate and add to collection")
+    parser.add_argument('-x', '--clear', action="store_true", help="Clear the collection. Use this without any other options.")
+    parser.add_argument('-p', '--print', action="store_true", help="Print additional information")
 
     args = parser.parse_args()
 
     if args.dburi:
         client = pymongo.MongoClient(args.dburi)
-        db = client["test"]
-        collection = db["users"]
+        db = client[args.dbname] # test
+        collection = db[args.collectionname] # users
         if collection is None:
             print("Couldn't find collection.")
             exit(-1)
@@ -54,14 +63,22 @@ def main():
             for document in collection.find():
                 pprint(document)
 
+    if args.clear:
+        collection.drop()
+        print("Collection erased.")
+        return
+
     count = 1
     if args.count:
         count = args.count
 
     data = gen_data(count)
 
-    print(data)
+    if args.print:
+        pprint(data)
     collection.insert_many(data)
+
+    print(f"Inserted {args.count} items into {args.collectionname}")
 
 if __name__ == "__main__":
     main()
